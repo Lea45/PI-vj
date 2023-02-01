8bf41bf913ace760a01766dc5ec2af9ded02b7e3
Lea45 <varzic.lea@gmail.com>
2023-02-01 21:33:43 +0100

    nizovi i objekti

--- 3. JS/vjezba.js
+++ 3. JS/vjezba.js
@@ -11,3 +11,21 @@ moj_niz.push("nesto novo");
 
 console.log(moj_niz);
 console.log(moj_niz.length);
+
+for (let x of moj_niz) {
+    console.log(x); 
+}
+
+
+let moj_objekt = {
+    ime: "Lea",
+    prezime: "Varžić",
+    godiste: 2000,
+    ispisi: function () {
+        console.log('asd');
+    },
+};
+
+console.log(moj_objekt);
+console.log(moj_objekt['ime']);
+console.log(moj_objekt.ime);
